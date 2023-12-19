18f19403ee7379f2f1a5f73bc8533dbe0c1d20aa
vergicore <147555888+vergicore@users.noreply.github.com>
2023-12-19 22:07:37 +0000

    learned events

--- events.js
+++ events.js
@@ -1,4 +1,4 @@
-// stored html elements
+
 let mario = document.querySelector(".mario");
 let coin = document.querySelector(".coin");
 let score = document.querySelector(".score");
@@ -11,56 +11,42 @@ let endGameButton = document.querySelector(".end-game");
 let updateScoreButton = document.querySelector(".update-score");
 let growButton = document.querySelector(".grow");
 
-/* 1. When jump button is clicked, Mario jumps and coin disappears.  
-
-________.addEventListener("click", function() {
+ jumpButton.addEventListener("click", function() {
     mario.style.bottom = "120px";
     coin.style.display = "none";
 });
 
-*/
 
-
-/* 2. When jump down button is clicked, Mario jumps down. 
-
-________.addEventListener("click", function() {
+jumpDownButton.addEventListener("click", function() {
     mario.style = "bottom: 35px";
 });
 
-*/
 
 
-/* 3. When change color button is clicked, background color changes. 
 
-________.addEventListener("click", function() {
-    background.style.backgroundColor = "springGreen"; // you can change the color to something different
+background.addEventListener("click", function() {
+    background.style.backgroundColor = "springGreen"; 
 });
 
-*/
 
 
-/* 4. When end game button is clicked, the title of the page says game over. 
 
-________._______ ("___", ________() {
+endGameButton.addEventListener("click", function() {
     title.innerHTML = "Game over.";
 });
 
-*/
 
 
-/* 5. When update score button is clicked, the counter score becomes "1" instead of 0. 
-
-________._______ ("___", ________() {
+updateScoreButton.addEventListener("click", function() {
     score.innerHTML = 1;
+    score.innerHTML = score.innerHTML + 1;
 });
 
-*/
 
 
-/* 6: When clicking the "Grow" button the Mario image gets bigger. Write the whole click handler! 
 
-{
+ growButton.addEventListener("click", function() {
     mario.style.width = "200px";
-}
+});
 
-*/
+
